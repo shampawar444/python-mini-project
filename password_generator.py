6730d0144616601d57fdcad480cbf71bc0bbd06d
Sham Pawar <khushalpawar016@gmail.com>
2024-09-11 15:54:24 +0530

    changes

--- password_generator.py
+++ password_generator.py
@@ -4,6 +4,10 @@ import string
 pass_len = 8
 charValues = string.ascii_letters + string.digits + string.punctuation
 
+# res = "".join([random.choice(charValues) for i in range(pass_len)])
+# print(res)
+
+
 password = ""
 for i in range(pass_len):
     password += random.choice(charValues)
